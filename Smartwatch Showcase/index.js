a5245e031bde299c16c8b1521adef9101d6d2e55
siddhi patel <patelsiddhi83@gmail.com>
2024-03-01 12:09:07 -0500

    project 4 done

---
+++ Smartwatch Showcase/index.js
@@ -0,0 +1,35 @@
+$('.order').click(function(e) {
+
+    let button = $(this);
+
+    if(!button.hasClass('animate')) {
+        button.addClass('animate');
+        setTimeout(() => {
+            button.removeClass('animate');
+        }, 10000);
+    }
+
+});
+
+var black_btn = document.querySelector('.black-button');
+var red_btn = document.querySelector('.red-button');
+var blue_btn = document.querySelector('.blue-button');
+var purple_btn = document.querySelector('.purple-button'); 
+var myImage = document.querySelector('.watch-img');
+
+black_btn.addEventListener('click' , function(){
+    myImage.src = './Black-img.png';
+});
+
+red_btn.addEventListener('click' , function(){
+    myImage.src = './Red-img.png';
+});
+
+blue_btn.addEventListener('click' , function(){
+    myImage.src = './Blue-img.png';
+});
+
+purple_btn.addEventListener('click' , function(){
+    myImage.src = './Purple-img.png';
+});
+
