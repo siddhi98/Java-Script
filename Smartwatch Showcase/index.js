270932194dd12127615eebcdf646ee32510fbea3
siddhi patel <patelsiddhi83@gmail.com>
2024-03-02 13:00:47 -0500

    Smartwatch showcase project 5 update

--- Smartwatch Showcase/index.js
+++ Smartwatch Showcase/index.js
@@ -38,3 +38,99 @@ purple_btn.addEventListener('click' , function(){
     myImage.src = './Purple-img.png';
 });
 
+
+
+// get time by 24 hr
+
+
+// function updateClock() {
+//     var currentTime = new Date();
+//     // Operating System Clock Hours for 12h clock
+//     var currentHoursAP = currentTime.getHours();
+//     // Operating System Clock Hours for 24h clock
+//     var currentHours = currentTime.getHours();
+//     // Operating System Clock Minutes
+//     var currentMinutes = currentTime.getMinutes();
+//     // Operating System Clock Seconds
+//     var currentSeconds = currentTime.getSeconds();
+//     // Adding 0 if Minutes & Seconds is More or Less than 10
+//     currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+//     currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+//     // Picking "AM" or "PM" 12h clock if time is more or less than 12
+//     var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+//     // transform clock to 12h version if needed
+//     currentHoursAP = (currentHours > 12) ? currentHours - 12 : currentHours;
+//     // transform clock to 12h version after mid night
+//     currentHoursAP = (currentHoursAP == 0) ? 12 : currentHoursAP;
+//     // display first 24h clock and after line break 12h version
+//     var currentTimeString = "24h kello: " + currentHours + ":" + currentMinutes + ":" + currentSeconds + "" + "<br>" + "12h kello: "    + currentHoursAP + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+//     // print clock js in div #clock.
+//     $("#clock").html(currentTimeString);}
+//     $(document).ready(function () {
+//     setInterval(updateClock, 1000);
+// });
+
+
+// get time by 12 hr
+
+
+function updateClock() {
+    var currentTime = new Date();
+    // Operating System Clock Hours for 12h clock
+    var currentHoursAP = currentTime.getHours();
+    // Operating System Clock Minutes
+    var currentMinutes = currentTime.getMinutes();
+    // Operating System Clock Seconds
+    var currentSeconds = currentTime.getSeconds();
+    // Adding 0 if Minutes & Seconds is More or Less than 10
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+    // Picking "AM" or "PM" 12h clock if time is more or less than 12
+    var timeOfDay = (currentHoursAP < 12) ? "AM" : "PM";
+    // // transform clock to 12h version if needed
+    // currentHoursAP = (currentHours > 12) ? currentHours - 12 : currentHours;
+    // // transform clock to 12h version after mid night
+    // currentHoursAP = (currentHoursAP == 0) ? 12 : currentHoursAP;
+    // // display first 12h clock
+    var currentTimeString = currentHoursAP + ":" + currentMinutes + ":" + currentSeconds + timeOfDay;
+    // print clock js in div #clock.
+    $("#clock").html(currentTimeString);    
+}
+$(document).ready(function () {
+    setInterval(updateClock, 1000);
+});
+
+
+// on click heart rate button show heart
+
+var heart_rate = document.querySelector('.heart-rate');
+var time = document.querySelector('.time');
+var clock_display = document.querySelector('#heart');
+var clock = document.querySelector('#clock');
+
+
+function heartRate(){
+    clock_display.innerHTML = "heart";
+    console.log("hello");
+    clock_display.classList.add('show');
+    clock_display.classList.remove('close');
+    clock.classList.remove('show');
+    clock.classList.add('close');
+
+
+};
+
+heart_rate.addEventListener('click', function(){
+    heartRate();
+});
+
+function timeEvent(){
+    clock_display.classList.add('close');
+    clock_display.classList.remove('show');
+    clock.classList.remove('close');
+    clock.classList.add('show');
+};
+
+time.addEventListener('click', function (){
+    timeEvent();
+});
