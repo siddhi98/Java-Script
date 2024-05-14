7566d8a7c6a60a7b2f312d616ce319864365cd97
siddhi patel <patelsiddhi83@gmail.com>
2024-05-14 16:56:15 -0400

    slider changes

--- Image Slider/index.js
+++ Image Slider/index.js
@@ -21,20 +21,29 @@ var next = document.querySelector('.next');
 
 // Function to change to the next slide
 next.addEventListener('click', function(){
-  console.log("next slide clicked");
-    currentSlide = (currentSlide + 1) % slides.length; // Increment current slide index
-    showSlide(currentSlide); // Show the new slide
+  nextSlide();
 });
 
 // Function to change to the previous slide
 prev.addEventListener('click', function(){
-  console.log("prev slide clicked");
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length; // Decrement current slide index
-    showSlide(currentSlide); // Show the new slide
+  prevSlide();
 });
+
+var nextSlide = function(){
+  currentSlide = (currentSlide + 1) % slides.length; // Increment current slide index
+  showSlide(currentSlide); // Show the new slide
+  console.log("currentSlide is" + currentSlide);
+  console.log("(currentSlide + 1) is" + (currentSlide + 1) );
+  console.log("(currentSlide + 1) % slides.length is" + (currentSlide + 1) % slides.length);
+};
+
+var prevSlide = function(){
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length; // Decrement current slide index
+  showSlide(currentSlide); // Show the new slide
+};
 
 // Initial call to show the first slide
 showSlide(currentSlide);
 
 // Automatic slide change every 3 seconds
-setInterval(nextSlide, 3000);
+// setInterval(nextSlide, 3000);
